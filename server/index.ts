e9d8e0d396844b947263a23e7d686288b2d97da4
Asif Ahmed <asifahmed715@gmail.com>
2025-09-30 23:50:31 +0500

    update cores

--- server/index.ts
+++ server/index.ts
@@ -2,10 +2,12 @@ import express, { type Request, Response, NextFunction } from "express";
 import session from "express-session";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
+import cors from "cors"; // 👈 imported
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors()); // 👈 use it here
 
 // Session configuration
 app.use(session({
