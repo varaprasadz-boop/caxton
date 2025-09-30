54219327df7d039d2886764198d2134aba891880
Asif Ahmed <asifahmed715@gmail.com>
2025-09-30 23:55:21 +0500

    update backend server

--- server/index.ts
+++ server/index.ts
@@ -2,7 +2,7 @@ import express, { type Request, Response, NextFunction } from "express";
 import session from "express-session";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
-import cors from "cors"; // 👈 imported
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
